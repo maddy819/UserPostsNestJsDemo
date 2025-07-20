029ada7f2af6d985b5d62027c0098959cf870fcc
Shubham Agrawal <vickya819@gmail.com>
2025-07-20 21:33:48 +0530

    Fourth commit

--- src/posts/post.schema.ts
+++ src/posts/post.schema.ts
@@ -1,16 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { Document } from 'mongoose';
 
 export type PostDocument = Post & Document;
 
 @Schema()
 export class Post {
+  @ApiProperty({
+    description: 'Title of the post',
+  })
   @Prop({ required: true })
   title: string;
 
+  @ApiProperty({
+    description: 'Content of the post',
+  })
   @Prop({ required: true })
   content: string;
 
+  @ApiProperty({
+    description: 'User ID of the post author',
+  })
   @Prop({ required: true })
   userId: string;
 }
--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,16 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
 @Schema()
 export class User {
+  @ApiProperty({
+    description: 'Name of the user',
+  })
   @Prop({ required: true })
   name: string;
 
+  @ApiProperty({
+    description: 'Email of the user',
+  })
   @Prop({ required: true, unique: true })
   email: string;
 
+  @ApiProperty({
+    description: 'Password of the user',
+  })
   @Prop({ required: true })
   password: string;
 }
